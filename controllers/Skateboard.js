c189c7c42e021d3427aa6adcd127f6adfa45a7a5
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-05-14 15:41:46 +0200

    SkateCRUD complete

---
+++ controllers/Skateboard.js
@@ -0,0 +1,32 @@
+import SkateModel from 'SkateboardModel'
+
+const SkateboardController = {
+    getAll(req, res) {
+        SkateModel.find() // Todos los productos. 
+        .then(skateboards => res.send(skateboards))
+        .catch(error => {
+            console.error(error);
+            res.send(error)
+        })
+    },
+    getById(req, res) {
+        SkateModel.findById(req.params._id)
+        .populate('userId')
+        .then(skateboard => res.send(skateboard))
+        .catch(error => {
+            console.error(error);
+            res.send(error);
+        })
+    },
+    SkateAdd(req, res) {
+        SkateModel.create(req.body)
+        .then(skateboard => res.status(201).send(skateboard))
+        .catch(error => {
+            console.error(error);
+            res.send(error);
+        })
+    }
+
+}
+
+export default SkateboardController;
